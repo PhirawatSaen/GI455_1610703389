cc1700f24d16b2d3a31a992d15273e8cbdf09c83
DESKTOP-O3H0ECD\User <phirawat.saen@bumail.net>
2021-01-26 02:47:12 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/Script/SearchName.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SearchName : MonoBehaviour
+{
+    public Text textColor;  
+    public Text inputField;
+    public Text textSearch;
+
+    
+
+
+    public void Searching()
+    {
+
+        if ((inputField.text == "Clock") || (inputField.text == "Computer") || (inputField.text == "Window") || (inputField.text == "Television") || (inputField.text == "Picture"))
+        {
+
+            
+            textSearch.GetComponent<Text>().text = inputField.text   + " is Found";
+            textColor.GetComponent<Text>().color = Color.green;
+
+        }
+        
+
+        else
+        {
+            
+            textSearch.GetComponent<Text>().text =  inputField.text + " is not Found";
+            textColor.GetComponent<Text>().color = Color.red;
+
+        }
+        
+        
+
+    }
+
+}
