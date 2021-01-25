b26f61ede4463420a8d031b90529637c873873e0
DESKTOP-O3H0ECD\User <phirawat.saen@bumail.net>
2021-01-25 07:09:14 +0700

    First Init Project

---
+++ GI455_Project/Assets/SearchName.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SearchName : MonoBehaviour
+{
+    public Text texts;
+    public string sName ;
+    public Text inputField;
+    public GameObject textSearch;
+
+    
+
+
+    public void Searching()
+    {
+       
+        if (sName == "Clock" )
+        {
+
+            sName = inputField.GetComponent<Text>().text;
+            textSearch.GetComponent<Text>().text = sName + " is Found";
+            texts.GetComponent<Text>().color = Color.green;
+            
+        }
+        
+
+        else if (sName == "Computer")
+        {
+            sName = inputField.GetComponent<Text>().text;
+            textSearch.GetComponent<Text>().text = sName + " is Found";
+            texts.GetComponent<Text>().color = Color.green;
+
+        }       
+
+        else if (sName == "Window")
+        {
+            sName = inputField.GetComponent<Text>().text;
+            textSearch.GetComponent<Text>().text = sName + " is Found";
+            texts.GetComponent<Text>().color = Color.green;
+        }
+
+        else if (sName == "Television")
+        {
+            sName = inputField.GetComponent<Text>().text;
+            textSearch.GetComponent<Text>().text = sName + " is Found";
+            texts.GetComponent<Text>().color = Color.green;
+        }
+
+        else if (sName == "Picture")
+        {
+            sName = inputField.GetComponent<Text>().text;
+            textSearch.GetComponent<Text>().text = sName + " is Found";
+            texts.GetComponent<Text>().color = Color.green;
+        }
+        else
+        {
+            sName = inputField.GetComponent<Text>().text;
+            textSearch.GetComponent<Text>().text = sName + " is not Found";
+            texts.GetComponent<Text>().color = Color.red;
+            
+        }
+        
+        
+
+    }
+
+}
